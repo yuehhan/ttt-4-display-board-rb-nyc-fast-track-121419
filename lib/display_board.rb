06e6e452717fd85574676210d5a545419764a5e8
Yueh Han <yuehwhan@gmail.com>
2019-12-15 15:03:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[3]} " + "|" +  " #{board[4]} " + "|" + " #{board[5]} "
   puts "-----------"
-
   puts "--------------"
   puts " #{board[3]} " + "|" +  " #{board[4]} " + "|" + " #{board[5]} "
   puts "--------------"
