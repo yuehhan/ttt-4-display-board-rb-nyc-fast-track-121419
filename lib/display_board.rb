6e8db4d2fe5332ac22470fc745b3625816634741
Yueh Han <yuehwhan@gmail.com>
2019-12-15 15:03:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,14 +3,12 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} " + "|" +  " #{board[1]} " + "|" + " #{board[2]} "
-<<<<<<< HEAD
   puts "-----------"
   puts " #{board[3]} " + "|" +  " #{board[4]} " + "|" + " #{board[5]} "
   puts "-----------"
-=======
+
   puts "--------------"
   puts " #{board[3]} " + "|" +  " #{board[4]} " + "|" + " #{board[5]} "
   puts "--------------"
->>>>>>> 2f9c0580f6f69ded13bed6c2333587995e049fd0
   puts " #{board[6]} " + "|" +  " #{board[7]} " + "|" + " #{board[8]} "
 end
